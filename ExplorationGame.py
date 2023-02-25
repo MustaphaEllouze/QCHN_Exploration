a9ec5a0c579e088ffc1485e7bbdae82c7f2cbefc
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-02-25 09:37:11 +0100

    feat/Constructeur ExploTerrain

--- ExplorationGame.py
+++ ExplorationGame.py
@@ -1,15 +1,27 @@
+from Utility import (
+    EDateTime,
+)
+
 class ExplorationTerrain : 
     """Représente un type de terrain différent pour le jeu
     """
     TERRAINS = {}
+
     def __init__(
             self,
-            duration,
-            fatigue,
-            hunger,
-            thirst,
+            name,
+            duration=1.0,
+            fatigue=1.0,
+            hunger=1.0,
+            thirst=1.0,
     ):
-        pass
+        self.name = name
+        self.duration = duration
+        self.fatigue = fatigue
+        self.hunger = hunger
+        self.thirst = thirst
+
+        ExplorationTerrain.TERRAINS[name]=self
 
 class ExplorationMap : 
     """Représente une carte
